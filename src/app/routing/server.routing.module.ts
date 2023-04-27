488657c1ca7cbeee2b26d2107395f427eade48b2
harit <harit123@gmail.com>
2023-04-27 03:39:32 -0700

    moved component into _practice-component

--- src/app/routing/server.routing.module.ts
+++ src/app/routing/server.routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServersComponent } from './servers/servers.component';
-import { UsersComponent } from '../users/users.component';
 import { HomeComponent } from './home/home.component';
 import { RoutingComponent } from './routing.component';
 import { UserComponent } from './routing-users/user/user.component';
