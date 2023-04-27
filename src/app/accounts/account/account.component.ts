edad38ac1ebf95b635da121351a7d11d9e022ff3
harit <harit123@gmail.com>
2023-04-27 02:46:49 -0700

    Code format

--- src/app/accounts/account/account.component.ts
+++ src/app/accounts/account/account.component.ts
@@ -1,28 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AccountService,UsersService } from 'src/app/_services';
+import { AccountService } from 'src/app/_services';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
- @Input() name!:string;
- @Input() status!:string;
- @Input() id!:number;
-  constructor(private accountService:AccountService) {}
+  @Input() name!: string;
+  @Input() status!: string;
+  @Input() id!: number;
+  constructor(private accountService: AccountService) {}
   ngOnInit(): void {
     // console.log(this.accountService.account);
   }
 
-testFunction(): void {
-  console.log('Hello')
-}
-  setToHello(status:string):void{
+  setToHello(status: string): void {
     console.log(status);
-    this.status=status;
+    this.status = status;
   }
 
-  onDelete():void{
+  onDelete(): void {
     this.accountService.onDelete(this.id);
   }
 }
